d0c633e0728c447523d3776cfe6a0101b1a945b7
ShahoudGradus <119983324+ShahoudGradus@users.noreply.github.com>
2025-02-25 01:30:17 +0300

    asdasd

--- app/(dashboard)/dashboard/loading.tsx
+++ app/(dashboard)/dashboard/loading.tsx
@@ -1,11 +1,23 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
   return (
     <div className="grid grid-cols-8 gap-3 p-10">
-      <div className="col-span-2 ">Loading 1</div>
-      <div className="col-span-2 ">Loading 2 </div>
-      <div className="col-span-2 ">Loading 3 </div>
-      <div className="col-span-2 ">Loading 4 </div>
-      <div className="col-span-8 ">Loading 5 </div>
+      <div className="col-span-2 ">
+        <Skeleton className=" h-[300px]" />
+      </div>
+      <div className="col-span-2 ">
+        <Skeleton className=" h-[300px]" />
+      </div>
+      <div className="col-span-2 ">
+        <Skeleton className=" h-[300px]" />
+      </div>
+      <div className="col-span-2 ">
+        <Skeleton className=" h-[300px]" />
+      </div>
+      <div className="col-span-8">
+        <Skeleton className="h-[250px]" />
+      </div>
     </div>
   );
 }
