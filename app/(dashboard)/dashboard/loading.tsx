676e55ed4b039430d9fe3d5bd2d6ac1fcdc124ec
ShahoudGradus <119983324+ShahoudGradus@users.noreply.github.com>
2025-02-25 01:19:00 +0300

    changed loading page

--- app/(dashboard)/dashboard/loading.tsx
+++ app/(dashboard)/dashboard/loading.tsx
@@ -1,3 +1,11 @@
 export default function Loading() {
-  return <div>Loading Payments data....</div>;
+  return (
+    <div className="grid grid-cols-8 gap-3 p-10">
+      <div className="col-span-2 ">Loading 1</div>
+      <div className="col-span-2 ">Loading 2 </div>
+      <div className="col-span-2 ">Loading 3 </div>
+      <div className="col-span-2 ">Loading 4 </div>
+      <div className="col-span-8 ">Loading 5 </div>
+    </div>
+  );
 }
